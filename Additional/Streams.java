4126fb023e1d2403f01eeb57fc717ec10f7c82e5
Mayank Saini <mayanksainisaini40@gmail.com>
2026-04-20 23:00:07 +0530

    I have made some changes

--- Additional/Streams.java
+++ Additional/Streams.java
@@ -1,5 +1,9 @@
 package Additional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Streams {
     public static void main(String[] args) {
         // feature introduced in Java 8
